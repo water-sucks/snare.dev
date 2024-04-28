990b9d5b7e04dc617cbf80571531f44d720d0e7b
Varun Narravula <varun@snare.dev>
2024-04-27 20:03:50 -0700

    feat: add sitemap generation

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [mdx(), icon()],
+  site: 'https://snare.dev',
+  integrations: [mdx(), icon(), sitemap()],
 });
