0c815ec693bbc42e3bfad79dd4db77990c590bb4
Varun Narravula <varun@snare.dev>
2025-01-14 13:56:42 -0800

    feat: use expressive-code for code blocks, fix code block overflow issues

--- astro.config.ts
+++ astro.config.ts
@@ -4,17 +4,29 @@ import icon from 'astro-icon';
 import sitemap from '@astrojs/sitemap';
 import darkroseSyntaxTheme from './src/styles/syntaxTheme.json';
 
+import expressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+
 export default defineConfig({
   site: 'https://snare.dev',
-  integrations: [mdx(), icon(), sitemap()],
+  integrations: [
+    expressiveCode({
+      themes: [darkroseSyntaxTheme],
+      plugins: [pluginLineNumbers()],
+      defaultProps: {
+        wrap: true,
+        overridesByLang: {
+          'bash,ps,sh': { preserveIndent: false },
+        },
+      },
+    }),
+    mdx(),
+    icon(),
+    sitemap(),
+  ],
   trailingSlash: 'never',
   build: {
     format: 'file',
-  },
-  markdown: {
-    shikiConfig: {
-      theme: darkroseSyntaxTheme,
-    },
   },
   image: {
     experimentalLayout: 'responsive',
