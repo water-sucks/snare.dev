9ea146d22d881281dfb5594ff1f536dbb087b256
Varun Narravula <varun@snare.dev>
2025-01-31 18:34:30 -0800

    fix(rss): use content collection rendering instead of markdown-it

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,30 +1,38 @@
-import rss from '@astrojs/rss';
+import rss, { type RSSFeedItem } from '@astrojs/rss';
 import type { APIRoute } from 'astro';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx';
+import { loadRenderers } from 'astro:container';
 import { getCollection } from 'astro:content';
-
 import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export const GET: APIRoute = async (context) => {
   const musings = await getCollection('musings');
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+
+  const items: Array<RSSFeedItem> = [];
+  for (const m of musings) {
+    const { Content } = await m.render();
+    const content = await container.renderToString(Content);
+    const { title, date, description } = m.data;
+
+    items.push({
+      content: sanitizeHtml(content, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+      title: title,
+      pubDate: new Date(date),
+      description: description,
+      link: `/musings/${m.slug}`,
+    });
+  }
 
   return rss({
     title: "Varun's Musings",
     description: 'A random man, on a journey of seeking knowledge',
     site: context.site || 'https://snare.dev',
     trailingSlash: false,
-    items: musings.map(({ body, data, slug }) => ({
-      content: sanitizeHtml(parser.render(body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-      }),
-      title: data.title,
-      pubDate: new Date(data.date),
-      description: data.description,
-      link: `/musings/${slug}`,
-      customData: `
-        <media:content xmlns:media="http://search.yahoo.com/mrss" url="${data.image.url}" medium="image" />
-      `,
-    })),
+    items: items,
   });
 };
